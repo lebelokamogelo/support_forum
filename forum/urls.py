d40720d765bd71487b5a5eefd8f867cfe0ece33a
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-02-09 23:35:09 +0200

    add topic and detail page

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('search/', views.search, name='search'),
     path('create/', views.create, name='create'),
+    path('topics/', views.my_topics, name='topics'),
+    path('topic/<str:uuid>/', views.detail_topic, name='detail-topic'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Blog
 
@@ -47,3 +48,18 @@ def create(request):
 
         return render(request, 'forum/create.html', context=context)
     return render(request, 'forum/create.html')
+
+
+@login_required(login_url="/auth/login")
+def my_topics(request):
+    data = Blog.objects.filter(author=request.user)
+
+    context = {"topics": data}
+    return render(request, 'forum/topics.html', context=context)
+
+
+def detail_topic(request, uuid):
+    blog = get_object_or_404(Blog, uuid=uuid)
+
+    context = {"blog": blog}
+    return render(request, 'forum/detail_topic.html', context=context)
