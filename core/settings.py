f0a440e695ce9589bbc94341125f93afa8a59b7d
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-02-17 02:35:35 +0200

    cache with redis

--- core/settings.py
+++ core/settings.py
@@ -89,6 +89,15 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
